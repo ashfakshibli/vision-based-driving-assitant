8d6379ebd7f04d6c63e813d87327c019c0bb5fbe
shibli16 <shibli.emon@gmail.com>
2017-08-04 15:59:49 +0600

    One file headpose working but ear not

--- deepgaze/attention_estimation.py
+++ deepgaze/attention_estimation.py
@@ -43,6 +43,42 @@ def eye_aspect_ratio(eye):
 	return ear
  
 
+
+
+#EAR Code
+frameNumber = 0
+earArray = []
+frameArray = []
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the
+# alarm
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48
+
+# initialize the frame counter as well as a boolean used to
+# indicate if the alarm is going off
+COUNTER = 0
+ALARM_ON = False
+
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+# predictor = dlib.shape_predictor(args["shape_predictor"])
+predictor = dlib.shape_predictor('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/shape_predictor_68_face_landmarks.dat')
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+(m,n) =(1,68)
+
+
 ######## Head Pose Part Code #########
 #
 #If True enables the verbose mode
@@ -80,6 +116,13 @@ TRACKED_POINTS = (0, 4, 8, 12, 16, 17, 26, 27, 30, 33, 36, 39, 42, 45, 62)
 ALL_POINTS = list(range(0,68)) #Used for debug only
 
 video_capture = cv2.VideoCapture(0)
+# time.sleep(1.0)
+
+
+
+f = open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'r+')
+f.truncate()
+
 
 
 def isRotationMatrix(R) :
@@ -111,7 +154,7 @@ def rotationMatrixToEulerAngles(R) :
 
 
 
-def headPose():
+def main():
 
     #Defining the video capture object
     # video_capture = cv2.VideoCapture(0)
@@ -214,8 +257,125 @@ def headPose():
 
         # Capture frame-by-frame
         ret, framePose = video_capture.read()
+        _,frame = video_capture.read() #ear
+        frame = imutils.resize(frame, width=450)
         framePose = imutils.resize(framePose, width=800)
         gray2 = cv2.cvtColor(framePose[roi_y1:roi_y2, roi_x1:roi_x2], cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #ear
+
+
+
+
+
+        # detect faces in the grayscale frame
+        rects = detector(gray2, 0)
+
+
+        # loop over the face detections
+        for rect in rects:
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+           
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            # extract the left and right eye coordinates, then use the
+            # coordinates to compute the eye aspect ratio for both eyes
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            mouth = shape[mStart:mEnd]
+            nose = shape[nStart:nEnd]
+            allpoints = shape[m:n]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            # average the eye aspect ratio together for both eyes
+            ear = (leftEAR + rightEAR) / 2.0
+            print(ear)
+
+            # frameNumber = frameNumber + 1
+            # plotAr = np.empty(1)
+            # plotArray = np.append(plotAr, [[frameNumber, ear]])
+
+            frameNumber = frameNumber + 1
+            frameArray.append(frameNumber)
+            earArray.append(ear)
+            len1 = len(frameArray)
+            len2 = len(earArray)
+
+            #print(earArray, frameArray)
+            #ani = animation.FuncAnimation(fig, animate, fargs=(frameArray, earArray),  interval=50, blit=True)
+            #plt.show()
+            #
+            file = open("/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt","a")
+            file.write(str(frameNumber) + "," + str(ear) +"\n")
+
+            if(frameNumber > 60):
+                with open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'r') as fin:
+                    data = fin.read().splitlines(True)
+                with open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'w') as fout:
+                    fout.writelines(data[1:])
+
+            
+
+
+            # print(plotArray)
+            #ani = animation.FuncAnimation(fig, animate, fargs=(frameArray, earArray),  interval=50)
+            #plt.show()
+
+            # compute the convex hull for the left and right eye, then
+            # visualize each of the eyes
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            mouthHull = cv2.convexHull(mouth)
+            noseHull = cv2.convexHull(nose)
+            allHull = cv2.convexHull(allpoints)
+            cv2.drawContours(framePose, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(framePose, [rightEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(framePose, [mouthHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(framePose, [noseHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(framePose, [allHull], -1, (0, 255, 0), 1)
+
+            # check to see if the eye aspect ratio is below the blink
+            # threshold, and if so, increment the blink frame counter
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
+
+                # if the eyes were closed for a sufficient number of
+                # then sound the alarm
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    # if the alarm is not on, turn it on
+                    if not ALARM_ON:
+                        ALARM_ON = True
+
+                        # check to see if an alarm file was supplied,
+                        # and if so, start a thread to have the alarm
+                        # sound played in the background
+                        # if args["alarm"] != "":
+                        t = Thread(target=sound_alarm,
+                                args=('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/alert.wav',))
+                        t.deamon = True
+                        t.start()
+
+                    # draw an alarm on the frame
+                    cv2.putText(framePose, "DROWSINESS ALERT!", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            # otherwise, the eye aspect ratio is not below the blink
+            # threshold, so reset the counter and alarm
+            else:
+                COUNTER = 0
+                ALARM_ON = False
+
+            # draw the computed eye aspect ratio on the frame to help
+            # with debugging and setting the correct eye aspect ratio
+            # thresholds and frame counters
+            cv2.putText(framePose, "EAR: {:.2f}".format(ear), (300, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+
 
 
         #Looking for faces with cascade
@@ -237,7 +397,7 @@ def headPose():
 
         #If any face is found for a certain
         #number of cycles, then the ROI is reset
-        if(no_face_counter == 50):
+        if(no_face_counter == 20):
             no_face_counter = 0
             roi_x1 = 0
             roi_y1 = 0
@@ -369,6 +529,7 @@ def headPose():
 
                 print(euler_angles)
 
+
                 yaw = math.degrees(euler_angles[1]);
                 pitch = math.degrees(euler_angles[0]);
                 roll = math.degrees(euler_angles[2]);
@@ -451,203 +612,20 @@ def headPose():
                          (0, 255, 255),
                          2)
 
+    
+
+        
+
         #Showing the framePose and waiting
+        #cv2.imshow("Frame", frame)
+        cv2.imshow("Video", framePose)
         # for the exit command
-        cv2.imshow('Video', framePose)
         if cv2.waitKey(1) & 0xFF == ord('q'): break
    
     #Release the camera
     video_capture.release()
     print("Bye...")
 
-######## Head Pose Part Code #########
+if __name__ == "__main__":
+    main()
 
-
-frameNumber = 0
-earArray = []
-frameArray = []
-
-# construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-p", "--shape-predictor", required=True,
-# 	help="path to facial landmark predictor")
-# ap.add_argument("-a", "--alarm", type=str, default="",
-# 	help="path alarm .WAV file")
-# ap.add_argument("-w", "--webcam", type=int, default=0,
-# 	help="index of webcam on system")
-# args = vars(ap.parse_args())
- 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold for to set off the
-# alarm
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 48
-
-# initialize the frame counter as well as a boolean used to
-# indicate if the alarm is going off
-COUNTER = 0
-ALARM_ON = False
-
-
-
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
-# predictor = dlib.shape_predictor(args["shape_predictor"])
-predictor = dlib.shape_predictor('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/shape_predictor_68_face_landmarks.dat')
-
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
-(m,n) =(1,68)
-
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-# vs = VideoStream(src=args["webcam"]).start()
-# vs = VideoStream(src=0).start()
-# time.sleep(1.0)
-
-
-f = open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'r+')
-f.truncate()
-
-# loop over frames from the video stream
-while True:
-	
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)
-	frame = video_capture.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
-
-	# loop over the face detections
-	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a np
-		# array
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
-
-		# extract the left and right eye coordinates, then use the
-		# coordinates to compute the eye aspect ratio for both eyes
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		mouth = shape[mStart:mEnd]
-		nose = shape[nStart:nEnd]
-		allpoints = shape[m:n]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
-
-		# frameNumber = frameNumber + 1
-		# plotAr = np.empty(1)
-		# plotArray = np.append(plotAr, [[frameNumber, ear]])
-
-		frameNumber = frameNumber + 1
-		frameArray.append(frameNumber)
-		earArray.append(ear)
-		len1 = len(frameArray)
-		len2 = len(earArray)
-
-		#print(earArray, frameArray)
-		#ani = animation.FuncAnimation(fig, animate, fargs=(frameArray, earArray),  interval=50, blit=True)
-		#plt.show()
-		#
-		file = open("/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt","a")
-		file.write(str(frameNumber) + "," + str(ear) +"\n")
-
-		if(frameNumber > 60):
-			with open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'r') as fin:
-			    data = fin.read().splitlines(True)
-			with open('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/test.txt', 'w') as fout:
-			    fout.writelines(data[1:])
-
-		
-
-
-		# print(plotArray)
-		#ani = animation.FuncAnimation(fig, animate, fargs=(frameArray, earArray),  interval=50)
-		#plt.show()
-
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		mouthHull = cv2.convexHull(mouth)
-		noseHull = cv2.convexHull(nose)
-		allHull = cv2.convexHull(allpoints)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [noseHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [allHull], -1, (0, 255, 0), 1)
-
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
-
-			# if the eyes were closed for a sufficient number of
-			# then sound the alarm
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				# if the alarm is not on, turn it on
-				if not ALARM_ON:
-					ALARM_ON = True
-
-					# check to see if an alarm file was supplied,
-					# and if so, start a thread to have the alarm
-					# sound played in the background
-					# if args["alarm"] != "":
-					t = Thread(target=sound_alarm,
-							args=('/home/ashfak/Desktop/DriversAssistanceSystem/deepgaze/FaceProject/alert.wav',))
-					t.deamon = True
-					t.start()
-
-				# draw an alarm on the frame
-				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-		# otherwise, the eye aspect ratio is not below the blink
-		# threshold, so reset the counter and alarm
-		else:
-			COUNTER = 0
-			ALARM_ON = False
-
-		# draw the computed eye aspect ratio on the frame to help
-		# with debugging and setting the correct eye aspect ratio
-		# thresholds and frame counters
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
- 
-	# show the frame
-	cv2.imshow("Frame", frame)
-	# t1 = Thread(target=headPose)
-	# t1.deamon = True
-	# t1.start()
-
-
-
-	
-	
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-
-
-# do a bit of cleanup
-
-cv2.destroyAllWindows()
-vs.stop()
